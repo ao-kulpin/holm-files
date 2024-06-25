c556902f181e69187ce22d0d274f11dc92aba385
Alexander Kulpin <alexander.kulpin7@gmail.com>
2024-06-25 15:38:27 +0300

    Swipers

---
+++ swiper1_2.js
@@ -0,0 +1,67 @@
+<script>
+const swiper = new Swiper('.swiperCarousel', {
+	slidesPerView: 4,
+  spaceBetween: 20,
+  loop: true,
+	breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+ 
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+		1024: {
+			slidesPerView: 3,
+      spaceBetween: 20
+		}
+	},
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+		scrollbar: {
+        el: '.swiper-scrollbar',
+        hide: true
+      }
+});
+	
+const swiper2 = new Swiper('.swiperGallery', {
+	slidesPerView: 2,
+  spaceBetween: 20,
+  loop: true,
+	breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+ 
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+		1024: {
+			slidesPerView: 2,
+      spaceBetween: 20
+		}
+	},
+  navigation: {
+    nextEl: '.gallery-next',
+    prevEl: '.gallery-prev'
+  },
+		scrollbar: {
+        el: '.swiper-scrollbar',
+        hide: true
+      }
+});
+</script>
